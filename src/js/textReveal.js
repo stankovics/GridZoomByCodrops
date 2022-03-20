6be294de0252474c7fc666b04a8b7de988b5f7ba
stankovics <stankovicnstefan@gmail.com>
2022-03-20 13:00:56 -0500

    out() method inside TextReveal class

--- src/js/textReveal.js
+++ src/js/textReveal.js
@@ -27,5 +27,18 @@ export class TextReveal {
       });
     return this.inTimeline;
   }
-  out() {}
+  out() {
+    if (this.inTimeline && this.inTimeline.isActive()) {
+      this.inTimeline.kill();
+    }
+
+    this.outTimeline = gsap
+      .timeline({ defaults: { duration: 0.5, ease: 'expo.in' } })
+      .to(this.DOM.inner, {
+        y: '-120%',
+        rotate: -5,
+        stagger: 0.03,
+      });
+    return this.outTimeline;
+  }
 }
