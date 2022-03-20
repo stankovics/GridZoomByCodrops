571de03ccb7e2f5d5f4031491e67fa9efa576e78
stankovics <stankovicnstefan@gmail.com>
2022-03-19 21:54:47 -0500

    adjustedBoundingRect

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -535,6 +535,8 @@ parcelHelpers.export(exports, "getScrollValues", ()=>getScrollValues
 );
 parcelHelpers.export(exports, "wrapLines", ()=>wrapLines
 );
+parcelHelpers.export(exports, "adjustedBoundingRect", ()=>adjustedBoundingRect
+);
 const imagesLoaded = require('imagesloaded');
 const preloadImages = (selector = 'img')=>{
     return new Promise((resolve)=>{
@@ -564,7 +566,7 @@ const calcWinsize = ()=>{
     const y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
     return y;
 };
-/**wrapLines
+/** wrapLines
  * wrap ecah element of an array
  * elems - array of elements to wrap
  * wrapType - type of wrapper (div, span...)
@@ -577,6 +579,42 @@ const calcWinsize = ()=>{
         char.parentNode.appendChild(wrapEl);
         wrapEl.appendChild(char);
     });
+};
+const adjustedBoundingRect = (el)=>{
+    let rect = el.getBoundingClientRect();
+    let style = getComputedStyle(el);
+    let tx = style.transform;
+    if (tx) {
+        let sx, sy, dx, dy;
+        if (tx.startsWith('matrix3d(')) {
+            let ta = tx.slice(9, -1).split(/, /);
+            sx = +ta[0];
+            sy = +ta[5];
+            dx = +ta[12];
+            dy = +ta[13];
+        } else if (tx.startsWith('matrix(')) {
+            let ta = tx.slice(7, -1).split(/, /);
+            sx = +ta[0];
+            sy = +ta[3];
+            dx = +ta[4];
+            dy = +ta[5];
+        } else return rect;
+        let to = style.transformOrigin;
+        let x = rect.x - dx - (1 - sx) * parseFloat(to);
+        let y = rect.y - dy - (1 - sy) * parseFloat(to.slice(to.indexOf(' ') + 1));
+        let w = sx ? rect.width / sx : el.offsetWidth;
+        let h = sy ? rect.height / sy : el.offsetHeight;
+        return {
+            x: x,
+            y: y,
+            width: w,
+            height: h,
+            top: y,
+            right: x + w,
+            bottom: y + h,
+            left: x
+        };
+    } else return rect;
 };
 
 },{"imagesloaded":"aYzyZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"aYzyZ":[function(require,module,exports) {
