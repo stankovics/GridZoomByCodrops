18d3b193f6de1efaf3713bf6f7af0ac2ec3892f0
stankovics <stankovicnstefan@gmail.com>
2022-03-23 20:14:09 -0500

    updated grid.js, declared DOM, and initial values

--- src/js/grid.js
+++ src/js/grid.js
@@ -20,5 +20,36 @@ window.addEventListener('resize', () => (winsize = calcWinsize()));
 
 export class Grid {
   // DOM elements
-  DOM = {};
+  DOM = {
+    // main element (.grid)
+    el: null,
+    // .grid__cell-img elements
+    imageCells: null,
+    // content section
+    content: null,
+    // .back button
+    backCtrl: null,
+    // The main mini grid shown in content area
+    miniGrid: {
+      //main element
+      el: null,
+      // cells
+      cells: null,
+    },
+  };
+  // ImageCell instances array
+  imageCellArr = [];
+  // Index of current imageCell
+  currentCell = -1;
+  // Checks if in view mode or if in content mode
+  isGridView = true;
+  // Checks for active animation
+  isAnimating = false;
+  // TextReveal obj to animate the text (slide in/out)
+  textReveal = null;
+  /**
+   * Constructor.
+   * @param {Element} DOM_el - the .grid--large element
+   */
+  constructor(DOM_el) {}
 }
